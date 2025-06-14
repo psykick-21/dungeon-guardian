b41dace3ce963eae1731912defbf409749d8be04
Gaurav Sharma <77984221+psykick-21@users.noreply.github.com>
2025-06-14 12:30:26 +0530

    Create type.py

---
+++ src/type.py
@@ -0,0 +1,24 @@
+from typing import TypedDict, Literal
+from enum import Enum
+
+class WorldState(TypedDict):
+    "Defines the structure for the world state"
+    health: int
+    stamina: int
+    potionCount: int
+    treasureThreatLevel: Literal['low', 'medium', 'high']
+    enemyNearby: bool
+    enemyLevel: Literal['very_low', 'low', 'medium', 'high', 'very_high', None]
+    isInSafeZone: bool
+    isBackup: bool
+
+
+class Goal(Enum):
+    "Enum for the goals of the agent"
+    SURVIVE = "survive"
+    ELIMINATE_THREAT = "eliminate_threat"
+    PROTECT_TREASURE = "protect_treasure"
+    PREPARE_FOR_BATTLE = "prepare_for_battle"
+
+
+# print(Goal._member_names_)
