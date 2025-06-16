3be584166cb0f1a5839954e4329028ea40455288
Gaurav Sharma <77984221+psykick-21@users.noreply.github.com>
2025-06-16 20:12:44 +0530

    renamed state variable

--- src/states.py
+++ src/states.py
@@ -9,7 +9,7 @@ class AgentState(TypedDict):
     current_world_state: WorldState
     previous_world_state: WorldState
     goals: Goals
-    suggestions: Union[str, None]
+    actionFailureSuggestions: Union[str, None]
     goal_justification: str
     action_sequence: list[str]
     planner_justification: str
