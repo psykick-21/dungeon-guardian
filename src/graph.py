d7aa49ba2d6fb3981601f639dcd157dc60d8d3c0
Gaurav Sharma <77984221+psykick-21@users.noreply.github.com>
2025-06-16 12:21:49 +0530

    Update graph.py

--- src/graph.py
+++ src/graph.py
@@ -5,9 +5,12 @@ from .states import AgentState
 from .nodes import (
     goal_generator_node,
     planner_node,
-    action_executor_node
+    action_executor_node,
+    check_success_conditions_node,
+    logger_node
 )
 from .type import WorldState
+from .routers import success_router
 
 
 
@@ -16,11 +19,16 @@ builder = StateGraph(AgentState)
 builder.add_node("goal_generator", goal_generator_node)
 builder.add_node("planner", planner_node)
 builder.add_node("action_executor", action_executor_node)
+builder.add_node("check_success_conditions", check_success_conditions_node)
+builder.add_node("logger", logger_node)
 
 builder.add_edge(START, "goal_generator")
 builder.add_edge("goal_generator", "planner")
 builder.add_edge("planner", "action_executor")
-builder.add_edge("action_executor", END)
+builder.add_edge("action_executor", "check_success_conditions")
+builder.add_edge("check_success_conditions", "logger")
+builder.add_edge("logger", END)
+# builder.add_conditional_edges("check_success_conditions", success_router)
 
 graph = builder.compile()
 
@@ -35,7 +43,9 @@ if __name__ == "__main__":
         enemyNearby=True,
         enemyLevel="medium",
         isInSafeZone=False,
-        isBackup=False
+        isBackup=False,
+        treasureHealth=60,
+        comfyActions=1
     )
     result = graph.invoke({"current_world_state": world_state})
     print(result)
